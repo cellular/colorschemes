e3ab0d4bef18eabbb3cf252d7f0d87bc50643c55
Felix Gnass <fgnass@cellular.de>
2020-08-03 14:24:53 +0200

    Updates

---
+++ precache-manifest.1e2cc5c6f7d4c0972c7b9f1dae76d236.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "598a37bbef558ddf90c42796d372d3e5",
+    "url": "/colorschemes/index.html"
+  },
+  {
+    "revision": "37d328ceb2170df1a31f",
+    "url": "/colorschemes/static/css/main.0a46cef8.chunk.css"
+  },
+  {
+    "revision": "6a57da3e0b8738ef70d8",
+    "url": "/colorschemes/static/js/2.0e973bdd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/colorschemes/static/js/2.0e973bdd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37d328ceb2170df1a31f",
+    "url": "/colorschemes/static/js/main.0a114fc4.chunk.js"
+  },
+  {
+    "revision": "2cd49f169ae817f1f350",
+    "url": "/colorschemes/static/js/runtime-main.e7db7f1b.js"
+  }
+]);
